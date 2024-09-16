5ebb69e4e435a322dc70dd8512608bbcc2bbba7f
Yamil <yamiyaluk@gmail.com>
2024-09-15 22:00:54 -0400

    finished
    comments left

--- lab4.c
+++ lab4.c
@@ -14,65 +14,91 @@ typedef struct {
 
 product_list list;
 
-// Función para verificar e ingresar enteros
+// Función para ingresar y verificar enteros
+// Parametros: El entero que se busca ingresar y los limites de su rango
+// Retorno: El entero ingresado y verificado correctamente
 int verificar(int num, int lim_inf, int lim_sup){
 	while (scanf("%d", &num) != 1 || getchar() != '\n') // Mientras no sea un entero el input o sea un salto de linea
         while (getchar() != '\n'); // Limpiar el buffer de entrada
-	while ((lim_inf == 0 ? 0 : num < lim_inf) || (lim_sup == 0 ? 0 : num > lim_sup)){
-		printf("Ingrese un numero entre %d y %d:\n", lim_inf, lim_sup);
+	while ((lim_inf == -1 ? 0 : num < lim_inf) || (lim_sup == -1 ? 0 : num > lim_sup)){
+		if (lim_inf != -1 && lim_sup == -1) printf("Ingrese un numero a partir de %d:\n", lim_inf);
+		if (lim_inf == -1 && lim_sup != -1) printf("Ingrese un numero hasta %d:\n", lim_sup);
+		if (lim_inf != -1 && lim_sup != -1) printf("Ingrese un numero entre %d y %d:\n", lim_inf, lim_sup);
         scanf("%d", &num);
         while (getchar() != '\n'); // Limpiar el buffer de entrada
     }
 	return num;
 }
 
-// Función para verificar e ingresar flotantes
+// Función para ingresar y verificar floats
+// Parametros: El float que se busca ingresar y los limites de su rango
+// Retorno: El float ingresado y verificado correctamente
 float verificar_f(float num){
-	while (scanf("%f", &num) != 1 || getchar() != '\n')
+	while (scanf("%f", &num) != 1 || getchar() != '\n' || num < 0)
         while (getchar() != '\n');
 	return num;
 }
 
 // Función para añadir productos
 void add_product(){
-	list.first_element = realloc(list.first_element, (list.product_num + 1) * sizeof(product));
+	char ch;
+	int size;
 	product new;
+	list.first_element = realloc(list.first_element, (list.product_num + 1) * sizeof(product));
 
-	if (list.first_element == NULL){
-		puts("Error en memoria.");
-		exit(1);
-	}
-	new.name = (char *)malloc(sizeof(char));
-
+	new.name = (char *)malloc(1);
 	puts("Ingrese el nombre del producto:");
-	scanf("%s", new.name);
+	for (size = 1; (ch = getchar()) != '\n'; size++) {
+        new.name = (char *)realloc(new.name, size * sizeof(char));
+        new.name[size - 1] = ch;
+    }
+	new.name[size - 1] = '\0';
 	puts("Ingrese el precio:");
 	new.price = verificar_f(new.price);
-	puts("Ingrese la cantidad");
-	new.quantity = verificar(new.quantity, 0, 0);
+	puts("Ingrese la cantidad:");
+	new.quantity = verificar(new.quantity, 1, -1);
 
 	list.first_element[list.product_num++] = new;
 }
 
 // Función para eliminar productos
 void delete_product(){
-	char *string;
-	puts("Que producto desea borrar?");
+	int index = 0;
+	product aux;
+	puts("Ingrese el indice del producto a borrar");
+	index = verificar(index, 0, list.product_num - 1);
+	for (int i = index; i < list.product_num - 1; i++){
+		aux = list.first_element[i];
+		list.first_element[i] = list.first_element[i + 1];
+		list.first_element[i + 1] = aux;
+	}
+	list.first_element = realloc(list.first_element, --list.product_num * sizeof(product));
 }
 
 // Función para editar productos
 void edit_product(){
-	char *string;
-	puts("Que producto desea modificar?");
-
+	int index = 0, size;
+	char ch;
+	puts("Ingrese el indice del producto a modificar");
+	index = verificar(index, 0, list.product_num - 1);
+	(list.first_element + index)->name = (char *)malloc(1);
+	puts("Ingrese el nombre del producto:");
+	for (size = 1; (ch = getchar()) != '\n'; size++) {
+		(list.first_element + index)->name = (char *)realloc((list.first_element + index)->name, size * sizeof(char));
+		(list.first_element + index)->name[size - 1] = ch;
+	}
+	(list.first_element + index)->name[size - 1] = '\0';
+	puts("Ingrese el nuevo precio:");
+	(list.first_element + index)->price = verificar_f((list.first_element + index)->price);
+	puts("Ingrese la nueva cantidad:");
+	(list.first_element + index)->quantity = verificar((list.first_element + index)->quantity, 1, -1);
 }
 
 // Función para mostrar todos los productos
 void show_products(){
 	puts("Los productos son:");
-	for (int i = 0; i < list.product_num; i++)
-		printf("%s ", (list.first_element + i)->name);
-    puts("");
+	for (int i = 0; i < list.product_num; i++, puts(""))
+		printf("Producto: %s -- Precio: %.2f -- Cantidad: %d", (list.first_element + i)->name, (list.first_element + i)->price, (list.first_element + i)->quantity);
 }
 
 // Función para calcular y mostrar el costo total
@@ -86,7 +112,8 @@ void total_cost(){
 int main(void) {
 	int option = 0;
 	while(1){
-		puts("\n--- Menu Principal ---\n1 -- Agregar un nuevo producto a la lista\n2 -- Eliminar un producto existente de la lista\n3 -- Editar la informacion de un producto en la lista\n4 -- Mostrar todos los productos presentes en la lista\n5 -- Calcular el costo total de todos los productos en la lista·\n0 -- Salir\n");
+		puts("\n--- Menu Principal ---\n1 -- Agregar un nuevo producto a la lista\n2 -- Eliminar un producto de la lista\n3 -- Editar la informacion de un producto en la lista");
+		puts("4 -- Mostrar todos los productos en la lista\n5 -- Calcular el costo total de todos los productos en la lista\n0 -- Salir\n");
 		option = verificar(option, 0, 5);
 		if (option == 1)
 			add_product();
@@ -98,6 +125,9 @@ int main(void) {
 			show_products();
 		else if (option == 5)
 			total_cost();
-		else return 0; // Salir del programa
+		else{
+			free(list.first_element);
+			return 0; // Salir del programa
+		}
 	}
 }
